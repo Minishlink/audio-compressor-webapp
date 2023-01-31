7c69617506c81815e855db009f6f5a5d7a3dfd24
Louis Lagrange <lagrange.louis@gmail.com>
2023-01-31 19:29:22 +0100

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Audio compressor</h1>
       <div className="card">
-        <p>
-          Compress your audio files locally on browser
-        </p>
+        <p>Compress your audio files locally on browser</p>
       </div>
     </div>
-  )
+  );
 }
-
