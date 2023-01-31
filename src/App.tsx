a7a78d9079428247e29d89915f45825ec5492a2a
Louis Lagrange <lagrange.louis@gmail.com>
2023-01-31 19:38:51 +0100

    Add audio file selection and listening

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import "./App.css";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [file, setFile] = useState<File | undefined>();
 
   return (
     <div className="App">
       <h1>Audio compressor</h1>
       <div className="card">
         <p>Compress your audio files locally on browser</p>
+        <input
+          type="file"
+          accept="audio/*"
+          onChange={(event) => setFile(event.target.files?.[0])}
+        />
+        {!!file && (
+          <>
+            <div id="player-container">
+              <audio controls src={URL.createObjectURL(file)}></audio>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
