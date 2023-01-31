6df058f7245b983e4d052e83f256cd27158289f0
Louis Lagrange <lagrange.louis@gmail.com>
2023-01-31 20:49:32 +0100

    Add actual compression

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,98 @@
 import { useCallback, useState } from "react";
-import "./App.css";
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-
-const ffmpeg = createFFmpeg({ log: true });
+import "./App.css";
 
 export function App() {
-  const [file, setFile] = useState<File | undefined>();
+  const [input, setInput] = useState<{ file: File; url: string } | undefined>();
+  const [progress, setProgress] = useState<number | undefined>();
+  const [error, setError] = useState<string | undefined>();
+  const [ffmpeg] = useState(() =>
+    createFFmpeg({
+      log: true,
+      progress: ({ ratio }) => setProgress(ratio * 100),
+    })
+  );
+
   const [output, setOutput] = useState<
     { url: string; name: string } | undefined
   >();
 
   const compress = useCallback(async () => {
-    if (!file) {
+    setProgress(undefined);
+    setError(undefined);
+    setOutput(undefined);
+
+    if (!input) {
       return;
     }
 
-    const { name } = file;
-    await ffmpeg.load();
-    ffmpeg.FS("writeFile", name, await fetchFile(file));
-    const outputFilename = `compressed_${Date.now()}_${name}`;
-    await ffmpeg.run("-i", name, outputFilename);
+    if (!ffmpeg.isLoaded()) {
+      await ffmpeg.load();
+    }
+
+    const { name } = input.file;
+    ffmpeg.FS("writeFile", name, await fetchFile(input.file));
+    const outputFilename = `${name}_compressed_${Date.now()}.ogg`;
+    await ffmpeg.run(
+      "-i",
+      name,
+      "-c:a",
+      "libopus",
+      "-b:a",
+      "32k",
+      "-vbr",
+      "on",
+      "-compression_level",
+      "10",
+      "-frame_duration",
+      "60",
+      "-application",
+      "voip",
+      outputFilename
+    );
     const data = ffmpeg.FS("readFile", outputFilename);
+    if (!data.length) {
+      setError("An unknown error occurred, see developer console");
+      return;
+    }
+
     setOutput({
-      url: URL.createObjectURL(new Blob([data.buffer], { type: "audio/m4a" })),
+      url: URL.createObjectURL(new Blob([data.buffer], { type: "audio/ogg" })),
       name: outputFilename,
     });
-  }, [file]);
+  }, [ffmpeg, input]);
 
   return (
     <div className="App">
       <h1>Audio compressor</h1>
-      <div className="card">
-        <p>Compress your audio files locally on browser</p>
+      <div>
+        <p>Compress your dialogue audio files locally on browser</p>
         <input
           type="file"
           accept="audio/*"
           onChange={(event) => {
-            setFile(event.target.files?.[0]);
+            setProgress(undefined);
+            setError(undefined);
             setOutput(undefined);
+
+            const file = event.target.files?.[0];
+            setInput(
+              file ? { file, url: URL.createObjectURL(file) } : undefined
+            );
           }}
         />
-        {!!file && (
+        {!!input && (
           <>
             <div className="player-container">
-              <audio controls src={URL.createObjectURL(file)} />
+              <audio controls src={input.url} />
             </div>
             <button onClick={compress}>Compress</button>
+            {!!progress && progress !== 100 && (
+              <div>
+                <progress max="100" value={progress}>{`${progress}%`}</progress>
+              </div>
+            )}
+            {!!error && <p className="error">{error}</p>}
           </>
         )}
         {!!output && (
